075d325d2329f33746994b9d407ad6c8d60bd655
Kayi <gpioblink@gmail.com>
2023-02-26 16:05:09 +0900

    add mapref

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,7 +1,11 @@
 <template>
-    <camera autoplay>
-        <button>I'm on top of the video</button>
-    </camera>
+    <v-container class="fill-height pl-0 pr-0" :style="{background: 'black'}" fluid>
+        <camera ref="camera" autoplay>
+            <v-responsive class="d-flex text-center align-end fill-height">
+                <v-btn>I'm on top of the video</v-btn>
+            </v-responsive>
+        </camera>
+    </v-container>
 </template>
 
 <script lang="ts" setup>
