b95397dca9a12c52e98c0346d8e73027d252ce51
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2023-03-05 13:43:01 +0900

    add: cameraConfirm

--- src/components/add/cameraConfirm.vue
+++ src/components/add/cameraConfirm.vue
@@ -1,10 +1,10 @@
 <template>
   <v-dialog
     v-model="dialog"
+    fullscreen
     activator="parent"
-    width="auto"
   >
-    <v-card width="w-80">
+    <v-card class="pt-5">
       <v-card-title>
         この画像で登録しますか？
       </v-card-title>
@@ -14,7 +14,8 @@
         :width="cameraSize.width / 1.3"
         :height="cameraSize.height / 1.3"
         cover
-        :src="props.url"
+        :src="props.image.url"
+        class="mx-auto"
       ></v-img>
       </v-card-item>
       
@@ -24,28 +25,50 @@
             <v-btn color="primary" block @click="dialog = false">再撮影</v-btn>
           </v-col>
           <v-col>
-            <v-btn color="primary" variant="outlined" block @click="dialog = false">登録</v-btn>
+            <v-btn @click="upload" :loading="uploadLoading" color="primary" variant="outlined" block>登録</v-btn>
           </v-col>
       </v-row>
       </v-card-actions>
     </v-card>
   </v-dialog>
+  <registerDialog :dialog="dialog2" :tags="tags" :cameraSize="cameraSize" :pic="pic" :blobUrl="props.image.url"/> 
 </template>
 
 
 <script lang="ts" setup>
-import { ref, onMounted, reactive } from "vue";
+import { ref, onMounted, reactive, watch } from "vue";
+import registerDialog from "./registerDialog.vue";
 
+interface Image {
+  url: string,
+  blob: Blob,
+}
 interface Props {
   dialog: boolean,
   cameraSize: {
     width: number,
     height: number,
   },
-  url : string,
+  image: Image,
 }
 const props = defineProps<Props>()
 const dialog = ref(props.dialog)
+const dialog2 = ref(false)
+const uploadLoading = ref(false)
+
+const tags = ref<string[]>([])
+const pic = ref<string>("")
+
+const upload = async () => {
+  uploadLoading.value = true
+  // ここで画像をアップロードする
+  await new Promise((resolve) => setTimeout(resolve, 1500))
+  tags.value = ["タグ1", "タグ2", "タグ3"];
+  pic.value = "https://picsum.photos/200/300";
+  dialog2.value = true
+  dialog.value = false
+  uploadLoading.value = false
+}
 
 </script>
   
